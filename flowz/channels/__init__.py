e6e558a8c63948719f056147a7260d5e068d7445
Patrick Rusk <PatrickDRusk@gmail.com>
2016-05-19 16:07:18 -0400

    FER-2844 Moving/renaming classes according to Ethan's comments.

--- flowz/channels/__init__.py
+++ flowz/channels/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-from .channels import (ChannelDone, Channel, ReadChannel, MapChannel, FlatMapChannel,
-                       FilterChannel, FutureChannel, ReadyFutureChannel, TeeChannel,
-                       ProducerChannel, IterChannel, ZipChannel, CoGroupChannel)
+from .core import (ChannelDone, Channel, ReadChannel, MapChannel, FlatMapChannel,
+                   FilterChannel, FutureChannel, ReadyFutureChannel, TeeChannel,
+                   ProducerChannel, IterChannel, ZipChannel, CoGroupChannel)
