9b261a4705228c3061c1bf0b90d9c0449ffbd3b0
Ethan Rowe <ethan@the-rowes.com>
2016-05-26 07:35:22 -0400

    ChannelManager class gives collection of accessors
    Adds `flowz.channels.management.ChannelManager` class which acts
    as a keyed collection of channel accessors.  A retrieval by key
    finds the respective accessor and returns the result of its `get()`.

--- flowz/channels/management.py
+++ flowz/channels/management.py
@@ -26,3 +26,27 @@ class ChannelAccessor(AbstractChannelAccessor):
         self.channel = self.builder()
         return self.channel
 
+
+class ChannelManager(object):
+    def __init__(self):
+        self.channels = {}
+
+    def add_builder(self, name, buildfunc):
+        bld = ChannelAccessor(buildfunc)
+        return self.add_accessor(name, bld)
+
+    def add_accessor(self, name, accessor):
+        self.channels[name] = accessor
+        return self
+
+    def add_channel(self, name, channel):
+        self.add_builder(name, lambda: channel)
+        return self
+
+    def __getitem__(self, key):
+        try:
+            accessor = self.channels[key]
+            return accessor.get()
+        except KeyError:
+            raise KeyError("No such key %s" % repr(key))
+
--- flowz/test/manager_test.py
+++ flowz/test/manager_test.py
@@ -39,3 +39,52 @@ def test_channel_accessor():
     tools.assert_equal(chan.tee.call2, accessor.get())
 
 
+def test_channel_manager_builders():
+    chan_a, chan_b = mock_channel(), mock_channel()
+    key_a, key_b = mock.Mock(), mock.Mock()
+    # Shows that generative syntax works by passing self along.
+    mgr = management.ChannelManager() \
+            .add_builder(key_a, lambda: chan_a) \
+            .add_builder(key_b, lambda: chan_b)
+
+    # Build/return on first access
+    tools.assert_equal(chan_a, mgr[key_a])
+    # Tee on subsequent access
+    tools.assert_equal(chan_a.tee.call0, mgr[key_a])
+    tools.assert_equal(chan_a.tee.call1, mgr[key_a])
+
+    tools.assert_equal(chan_b, mgr[key_b])
+    tools.assert_equal(chan_b.tee.call0, mgr[key_b])
+    tools.assert_equal(chan_b.tee.call1, mgr[key_b])
+
+
+def test_channel_manager_accessors():
+    access_a, access_b = mock.Mock(), mock.Mock()
+    key_a, key_b = mock.Mock(), mock.Mock()
+
+    # Shows chaining syntax
+    mgr = management.ChannelManager() \
+            .add_accessor(key_a, access_a) \
+            .add_accessor(key_b, access_b)
+
+    # Get access delegates to the accessor's get()
+    tools.assert_equal(access_a.get.return_value, mgr[key_a])
+    tools.assert_equal(access_b.get.return_value, mgr[key_b])
+
+
+def test_channel_manager_channels():
+    chan_a, chan_b = mock_channel(), mock_channel()
+    key_a, key_b = mock.Mock(), mock.Mock()
+
+    mgr = management.ChannelManager() \
+            .add_channel(key_a, chan_a) \
+            .add_channel(key_b, chan_b)
+
+    tools.assert_equal(chan_a, mgr[key_a])
+    tools.assert_equal(chan_b, mgr[key_b])
+    tools.assert_equal(chan_a.tee.call0, mgr[key_a])
+    tools.assert_equal(chan_b.tee.call0, mgr[key_b])
+    tools.assert_equal(chan_a.tee.call1, mgr[key_a])
+    tools.assert_equal(chan_b.tee.call1, mgr[key_b])
+
+
